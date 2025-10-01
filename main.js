6d126c7f4ddb40448fe4510050540d2853967e84
Akanksha Pingale <akankshapingale1407@gmail.com>
2025-10-01 10:48:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const navlist=document.getElementById('navlist')
+const hamburger=document.getElementById('hamburger')
+
+
+hamburger.addEventListener('click',()=>{
+  navlist.classList.toggle('navlist-active')
+})
+ 
